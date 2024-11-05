fc98e99b94dd3f1b9f106208cdb36d5d66918903
lukemcdowell <luke.db.mcdowell@gmail.com>
2024-11-05 17:08:09 +0000

    emailing success message with link to playlist

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,20 @@
+from datetime import datetime, timedelta
 import json
+from mail import send_email
 from main import create_on_repeat_archive
 
 
 def lambda_handler(event, context):
+    today = datetime.now()
+    last_month = today - timedelta(weeks=1)
+    playlist_name = last_month.strftime("%b %y")
+
     try:
-        create_on_repeat_archive()
-        # TODO: email on success
+        playlist_url = create_on_repeat_archive(playlist_name)
+        send_email(
+            "On Repeat Archive",
+            f"On Repeat archive created: <a href={playlist_url}>{playlist_name}</a>",
+        )
 
         return {"statusCode": 200, "body": json.dumps("Playlist created successfully!")}
     except Exception as e:
@@ -13,3 +22,7 @@ def lambda_handler(event, context):
             "statusCode": 500,
             "body": json.dumps(f"Failed to create playlist: {str(e)}"),
         }
+
+
+if __name__ == "__main__":
+    print(lambda_handler("test", "test"))
